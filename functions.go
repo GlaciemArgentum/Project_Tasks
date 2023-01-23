773c394e44c3cd28be6a9928f296481bd2068834
Trofimov Egor <egor@6693790.ru>
2023-01-23 23:10:04 +0300

    update

--- functions.go
+++ functions.go
@@ -4,6 +4,8 @@ import (
 	_ "database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"regexp"
+	"time"
 )
 
 type Event struct {
@@ -13,8 +15,27 @@ type Event struct {
 	description string
 }
 
-func CheckErr(err error, log string) {
+func CheckErr(err error, log string) bool {
 	if err != nil {
-		fmt.Println(fmt.Errorf("%s: %w\n", log, err))
+		fmt.Println(fmt.Errorf("%s: %w", log, err))
+		return true
 	}
+	return false
+}
+
+func CheckDataFormat(date, format string, log string) bool {
+	_, err := time.Parse(format, date)
+	if err != nil {
+		fmt.Println(fmt.Errorf("%s", log))
+		return true
+	}
+	return false
+}
+
+func CheckLogic(str string, sample string, log string) bool {
+	if !regexp.MustCompile(sample).MatchString(str) {
+		fmt.Println(fmt.Errorf("%s", log))
+		return true
+	}
+	return false
 }
--- main.go
+++ main.go
@@ -11,24 +11,74 @@ import (
 
 func main() {
 	db, err := sql.Open("mysql", "root:12345678@tcp(127.0.0.1:3306)/db")
-	//defer CheckErr(db.Close(), "Ошибка закрытия БД")
-	CheckErr(err, "Ошибка открытия БД")
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			fmt.Printf("Ошибка закрытия БД: %v\n", err)
+		}
+	}(db)
+	if CheckErr(err, "Ошибка открытия БД") {
+		panic("panic")
+	}
 
 	in := bufio.NewReader(os.Stdin)
 	var input string
+
 	for {
-		input, _ = in.ReadString('\n')
-		switch {
+		input, err = in.ReadString('\n')
+		input = strings.ReplaceAll(input, "\n", "")
+		if CheckErr(err, "Ошибка чтения in.ReadString") {
+			continue
+		}
 
-		case strings.HasPrefix(input, "/addEvent "):
+		switch input {
+		case `/addEvent`:
 			var event Event
-			_, err := fmt.Sscanf(input, "/addEvent %s %s %s %s", &event.name, &event.dataTime, &event.duration, &event.description)
-			CheckErr(err, "Ошибка Sscanf")
-			event.dataTime = strings.Replace(event.dataTime, "T", " ", 1)
-			_, err = db.Exec(fmt.Sprintf("INSERT INTO table_of_events(time_begin, duration, name, description) VALUES ('%s', '%s', '%s', '%s')", event.dataTime, event.duration, event.name, event.description))
-			CheckErr(err, "Ошибка добавления данных в БД")
+			fmt.Print("Введите данные\nНазвание: ")
+			event.name, err = in.ReadString('\n')
+			event.name = strings.ReplaceAll(event.name, "\n", "")
+			if CheckErr(err, "Ошибка чтения in.ReadString") || CheckLogic(event.name, `^.{1,}$`, "Название должно содиржать хотя бы один символ") {
+				break
+			}
 
-		case strings.HasPrefix(input, "/findEvent id "):
+			fmt.Print("Дата: ")
+			event.dataTime, err = in.ReadString('\n')
+			event.dataTime = strings.ReplaceAll(event.dataTime, "\n", "")
+			if CheckErr(err, "Ошибка чтения in.ReadString") || CheckDataFormat(event.dataTime, "02.01.2006", "Неверный формат даты") {
+				break
+			}
+			dataSlice := strings.Split(event.dataTime, ".")
+			event.dataTime = dataSlice[2] + "-" + dataSlice[1] + "-" + dataSlice[0]
+
+			fmt.Print("Время: ")
+			time, err := in.ReadString('\n')
+			time = strings.ReplaceAll(time, "\n", "")
+			if CheckErr(err, "Ошибка чтения in.ReadString") || CheckDataFormat(time, "15:04", "Неверный формат времени") {
+				break
+			}
+			event.dataTime += " " + time + ":00"
+
+			fmt.Print("Продолжительность: ")
+			event.duration, err = in.ReadString('\n')
+			event.duration = strings.ReplaceAll(event.duration, "\n", "")
+			if CheckErr(err, "Ошибка чтения in.ReadString") || CheckDataFormat(event.duration, "15:04", "Неверный формат продолжительности") {
+				break
+			}
+			event.duration += ":00"
+
+			fmt.Print("Описание: ")
+			event.description, err = in.ReadString('\n')
+			event.description = strings.ReplaceAll(event.description, "\n", "")
+			if CheckErr(err, "Ошибка чтения in.ReadString") {
+				break
+			}
+
+			_, err = db.Exec(fmt.Sprintf("INSERT INTO table_of_events(time_begin, duration, name, description) VALUES ('%s', '%s', '%s', '%s')", event.dataTime, event.duration, event.name, event.description))
+			if CheckErr(err, "Ошибка добавления данных в БД") {
+				break
+			}
+
+		case `/findEvent -id`:
 			var id int
 			_, err := fmt.Sscanf(input, "/findEvent id %d", id)
 			CheckErr(err, "Ошибка Sscanf")
@@ -37,14 +87,14 @@ func main() {
 	}
 }
 
-// /addEvent Встреча 2023-12-23T05:50:00 02:30:00 Описание
+// /addEvent Встреча 20.09.2000 02:30 Описание
 
-// /showEvent id 1
-// /showEvent dataTime 2023-12-23T05:50:00
-// /showEvent interval 2023-12-23T05:50:00 2023-12-23T05:55:00
+// /showEvent -id 1
+// /showEvent -dataTime 2023-12-23T05:50:00
+// /showEvent -interval 2023-12-23T05:50:00 2023-12-23T05:55:00
 
-// /deleteEvent id 1
-// /deleteEvent dataTime 2023-12-23T05:50:00
+// /deleteEvent -id 1
+// /deleteEvent -dataTime 2023-12-23T05:50:00
 
 // /addList Список покупок
 
